512f0c461eba7423668dc1098fbc8ddfa8941d26
shiho imamura <zhibao29@yahoo.co.jp>
2022-12-19 18:22:43 +0900

    test

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"ブログを削除しました！"
+  end
+
+  def create
+    Post.create(content: params[:post][:content])
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "ブログを作成しました！"
+      else
+        render :new
+      end
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+    
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+end
---
+++ test/controllers/clones_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class ClonesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get clones_index_url
+    assert_response :success
+  end
+end
